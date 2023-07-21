165d3b73a7b702fb76730b953880eedfd1936eb6
Denny Daffa Rizaldy <dennydaffarizaldy16@gmail.com>
2023-07-21 19:01:23 +0700

    Tensorflow

--- index.py
+++ index.py
@@ -1,56 +1,65 @@
-import cv2
-import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
-def count_detected_black_shrimp_seed(image_path, sample_paths):
-    # Load the test image
-    image = cv2.imread(image_path)
+# Definisi parameter untuk model CNN
+batch_size = 32
+epochs = 10
+input_shape = (150, 150, 3)  # Ubah sesuai ukuran gambar Anda
 
-    # Convert the test image to grayscale
-    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Definisi model CNN
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(1, activation='sigmoid')  # Binary classification (udang atau bukan udang)
+])
 
-    # Initialize a variable to count the total detected black shrimp seeds
-    total_detected_shrimp_seeds = 0
+# Kompilasi model
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
 
-    for sample_path in sample_paths:
-        # Load the sample image
-        sample = cv2.imread(sample_path)
+# Preprocessing dan Augmentasi data
+train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+train_generator = train_datagen.flow_from_directory(
+    'D:\\BISA!\\Project-IOT-Udang\\data_latihan',  # Gunakan double backslash
+    target_size=input_shape[:2],
+    batch_size=batch_size,
+    class_mode='binary'
+)
 
-        # Convert the sample image to grayscale
-        grayscale_sample = cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)
 
-        # Get the dimensions of the sample image
-        sample_height, sample_width = grayscale_sample.shape[::-1]
+# Latih model
+model.fit(train_generator, epochs=epochs)
 
-        # Perform template matching
-        result = cv2.matchTemplate(grayscale_image, grayscale_sample, cv2.TM_CCOEFF_NORMED)
+# Simpan model
+model.save('shrimp_detection_model.h5')
 
-        # Set a threshold for considering a match
-        threshold = 0.8
+# Load model yang telah dilatih jika perlu
+# model = tf.keras.models.load_model('shrimp_detection_model.h5')
 
-        # Find locations where the result is above the threshold
-        loc = np.where(result >= threshold)
+# Sekarang, Anda dapat menggunakan model untuk mendeteksi bibit udang dalam gambar test.jpg
+# Misalnya:
+from PIL import Image
 
-        # Draw rectangles around the detected regions
-        for pt in zip(*loc[::-1]):
-            cv2.rectangle(image, pt, (pt[0] + sample_width, pt[1] + sample_height), (0, 0, 255), 2)
-            total_detected_shrimp_seeds += 1
+# Load gambar test
+test_image = Image.open('test.jpg')
+test_image = test_image.resize(input_shape[:2])  # Resize gambar sesuai input_shape model
+test_array = tf.keras.preprocessing.image.img_to_array(test_image)
+test_array = test_array[np.newaxis, ...]  # Tambahkan dimensi batch
 
-    # Display the result image with detected black shrimp seeds
-    cv2.imshow("Detected Black Shrimp Seeds", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+# Lakukan prediksi
+prediction = model.predict(test_array)
 
-    return total_detected_shrimp_seeds
-
-if __name__ == "__main__":
-    image_path = "test.png"  # Ganti dengan nama file gambar yang ingin dideteksi
-
-    sample_paths = [
-        "sample1.png",
-        "sample2.png",
-        "sample3.png",
-        "sample4.png"
-    ]  # Ganti dengan daftar nama file gambar sampel
-
-    detected_count = count_detected_black_shrimp_seed(image_path, sample_paths)
-    print(f"Jumlah bibit udang yang terdeteksi: {detected_count}")
+# Jika nilai prediksi mendekati 1, maka itu merupakan bibit udang
+if prediction[0][0] > 0.5:
+    print("Bibit udang terdeteksi!")
+else:
+    print("Bukan bibit udang.")
